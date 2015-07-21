8832771b4646d3500d0cc49e893e1543978097bf
sword36 <sword36rus@gmail.com>
2015-07-21 03:07:45 +0300

    try to add serialport to node webkit
    Former-commit-id: 61f81210e9c97ce315f41c165fade9345fe88600

--- breathe/src/js/game.js
+++ breathe/src/js/game.js
@@ -1,1 +1,11 @@
-var world = require("./world.js")();
+global.document = window.document;
+global.Image = window.Image;
+global.Audio = window.Audio;
+global.reaquestAnimationTime = window.reaquestAnimationTime;
+global.localStorage = window.localStorage;
+console.log(process.versions["node-webkit"]);
+var world = require("./src/js/world.js")();
+var SerialPort = require("serialport").SerialPort;
+var serialPort = new SerialPort("COM5", {
+
+});

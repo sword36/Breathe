c8d140c00c6674aa2825b8c9019ff5a90740d3d0
sword36 <sword36rus@gmail.com>
2015-08-17 02:37:21 +0300

    fixed old bug second time: app should named nw.exe

--- gruntfile.js
+++ gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         nwjs: {
             options: {
-                platforms: ['win'],
+                platforms: ['win64'],
                 buildDir: './webkitbuilds', // Where the build version of my NW.js app is saved
                 winIco: 'breathe/img/breathe.ico'
             },
--- nwbuilder.js
+++ nwbuilder.js
@@ -5,7 +5,7 @@ var NwBuilder = require('node-webkit-builder');
 var nw = new NwBuilder({
     files: './breathe/**/**', // use the glob format
     platforms: ['win64'],
-    gypModules: ['gyp', 'zmq', './breathe/node_modules/serialport/build/serialport/v1.7.4/Release/node-v14-win32-x64/serialport']
+    gypModules: ['gyp', 'zmq', './breathe/node_modules/serialport/build/serialport/v1.7.4/Release/node-webkit-v0.12.3-win32-x64/serialport']
 });
 
 //Log stuff you want

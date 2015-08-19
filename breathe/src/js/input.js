2e8ab5c6342eda4977b2a69abdad21bdde06dde4
sword36 <sword36rus@gmail.com>
2015-08-20 01:02:24 +0300

    serialport updata: check for disconnect every sec added

--- breathe/src/js/input.js
+++ breathe/src/js/input.js
@@ -9,14 +9,29 @@ var config = require("./config.js");
 function input() {    //type - keyboard, medicine, smartphone
     "use strict";
     var pressed = null;
+    var lastDataTime = 0;
+
+
+    function checkDataTime() {
+        if (Date.now() - lastDataTime > 1000) {
+            console.log("No devices");
+            localStorage.setItem("errorMessage", "Ошибка: соединение с устройством потеряно. Попробуйте переподключить его.");
+            serialPortStorageSingleton.onErrorCallbacks.forEach(function(func) {
+                func();
+            });
+        }
+    }
 
     if (config.inputType === "serialport") {
-        var SerialPortStorage = require("./serialPortStorage.js");
-        var serialPortStorage = SerialPortStorage();
+        var serialPortStorageSingleton = require("./serialPortStorage.js");
+        var serialPortStorage = serialPortStorageSingleton();
 
         var port = serialPortStorage.port;
         port.removeAllListeners();
+
+
         port.on('data', function(data) {
+            lastDataTime = Date.now();
             data = data.toString();
             var splitDataAll = data.split("\r\n");
             var splitDataCur = splitDataAll[1];
@@ -30,18 +45,10 @@ function input() {    //type - keyboard, medicine, smartphone
                 console.log(data);
                 pressed.breathe = data;
             }
+            //port.write("s");
         });
 
-        port.on("disconnect", function(err) {
-            debugger;
-        });
-        port.on("close", function(err) {
-            debugger;
-        });
-        port.options.disconnectedCallback = function(err) {
-            debugger;
-        };
-        debugger;
+        var checkDataInterval = setInterval(checkDataTime, 1000);
     }
 
     function handler(event) {
